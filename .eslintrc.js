d8c49d7aff822c956ff2f5bfb789fe1f1357e9fb
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-05-31 12:47:57 +0200

    Add custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,17 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-underscore-dangle": ["error", { "allowAfterThis": true }],
+    "brace-style": ["error", "stroustrup", { "allowSingleLine": true }],
+    "max-len": ["error", 150],
+    "computed-property-spacing": ["error", "always"],
+    "no-unused-vars": ["error", { "args": "none" }],
+    "no-param-reassign": ["error", { "props": false }],
+    "space-in-parens": ["error", "always"],
+    "array-bracket-spacing": ["error", "always"],
+    "no-plusplus": 0,
+    "indent": "off"
   },
   parserOptions: {
     parser: 'babel-eslint'
---
+++ src/Layouts/main.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <h1>container</h1>
+    <slot name="main"></slot>
+  </div>
+</template>
+<script></script>
+<style lang="stylus"></style>
