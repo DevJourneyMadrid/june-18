1940a57e4e224f3e628162e7586ac4049fb0f35d
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-06-03 19:28:03 +0200

    Initial commit

---
+++ src/components/ImageBlock.vue
@@ -0,0 +1,55 @@
+<template>
+  <header class="header">
+    <div class="title-container pl-4 pr-4">
+      <h1 class="fw-900" v-if="text.title">{{ text.title }}</h1>
+      <h2 v-if="text.subtitle">{{ text.subtitle }}</h2>
+    </div>
+  </header>
+</template>
+<script>
+  export default {
+    name: 'ImageBlock',
+    props: {
+      imagePath: {
+        required: false,
+        type: String,
+      },
+      text: {
+        required: false,
+        type: Object,
+      },
+    },
+    data() {
+      return {
+        msg: 'Hola',
+      };
+    },
+  };
+</script>
+<style lang="stylus" scoped>
+  .header
+    position relative
+    background url("../assets/img/bg1.jpg") no-repeat center center
+    -webkit-background-size: cover
+    background-size: cover
+    display flex
+    height 200px
+    text-align center
+    align-items center
+    &:after
+      content ''
+      position absolute
+      width 100%
+      height 100%
+      background-color black
+      opacity .5
+    .title-container
+      display block
+      width 100%
+      z-index: 1;
+      color white
+      text-shadow: 1px 2px 4px #777;
+      .fw-900
+        font-weight 900
+        letter-spacing 1px
+</style>
---
+++ src/views/About/TextBlock.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="text-block">
+    <div class="container">
+      <div class="wrapper mt-5 mb-5">
+        <p>
+          Para esta edición desde la <a href="https://openexpoeurope.com/es/" target="_blank">Open Expo</a>, han
+          confiado la representación de las comunidades tecnológicas a Open Source Weekends que se ofrecieron a
+          organizar un <b>Open Communities Summit</b> durante el día y medio
+          que dura el evento.
+        </p>
+        <h3 id="¿qué-es-open-source-weekends">¿Qué es Open Source Weekends?</h3>
+        <p>OSW es una comunidad tecnológica que promueve el intercambio de conocimiento y la colaboración, lo que se
+          gestó desde su creación. Uno de los fundadores, Jose, era jefe del equipo de seguridad de Campus y <a
+            href="https://www.elconfidencial.com/tecnologia/2017-05-23/guardia-seguridad-google-programador-desarrollador-startup_1385286/">siempre
+            quiso convertirse en desarrollador</a>. Al conocer a <a href="https://twitter.com/CodingCarlos">Carlos
+            Hernández</a> y <a href="https://twitter.com/kom_256">Ulises Gascón</a> en <a
+            href="https://twitter.com/CampusMadrid">Campus Madrid</a>, encontró a grandes mentores con los que
+          transformar su vida, especialmente al crear juntos una comunidad que representara sus valores.</p>
+        <p>Además de las 2500 personas que se han unido a la comunidad desde sus inicios, muchas otras comunidades se
+          han hermanado con OSW para impulsar la colaboración, sinergias y apoyo entre ellas. Así han conseguido crear
+          un ecosistema alrededor de la Cultura Open Source fundamentado en el desarrollo de proyectos con impacto
+          social en áreas como la agricultura, la educación, … hasta los datos abiertos, Blockchain y
+          Ciberseguridad.</p>
+        <p>La comunidad siempre está abierta y es inclusiva con todo tipo de perfiles, tanto por sector como por nivel
+          de conocimientos. Muchas mujeres del mundo tecnológico y de otros sectores han encontrado en OSW un espacio en
+          el que aprender y crecer como profesionales, lo cual ha nacido de manera orgánica al existir libertad y
+          respeto hacia todas las personas, nunca predeterminado ni forzado por los miembros de la organización, lo que
+          añade más autenticidad a todo lo que surge alrededor del tema de la diversidad e inclusión.</p>
+        <h3 id="¿qué-se-hará-durante-el-ocsummit">¿Qué se hará durante el OCSummit?</h3>
+        <p>Un Open Community Summit es un evento de comunidades donde Open Source Weekends invita a las comunidades
+          tecnológicas más destacadas del panorama a exponer lo que hacen durante una tarde.</p>
+        <p>En esta ocasión contaremos con <a href="https://twitter.com/HaskellMAD">HaskellMAD</a>, <a
+          href="https://twitter.com/hackmadrid">Hack Madrid %27</a>, <a href="https://twitter.com/PyLadiesMadrid">PyLadies
+          Madrid</a>, <a href="https://twitter.com/RLadiesMAD">RLadies Madrid</a>, <a
+          href="https://twitter.com/madrustaceans">Madrid Rust</a>, <a href="https://twitter.com/MadridScala">Madrid
+          Scala</a>, <a href="https://twitter.com/MakespaceMadrid">Makespace Madrid</a>, <a
+          href="https://twitter.com/codenares">Codenares</a>, <a href="https://twitter.com/madridtechall">Madrid Tech
+          Alliance</a> y muchas más…</p>
+        <p>Open Source Weekends tendrá un track permanente en el Demo Corner del Communtiy Village donde se harán varias
+          presentaciones sobre el trabajo realizado en proyectos Open Source por parte la comundiad de OSW, los
+          #OSWGuilds.<br>
+          Iniciativas como <a href="https://github.com/GingerCode">Ginger Code</a>, <a
+            href="https://github.com/Fictizia/aireMAD">AireMAD</a>, <a
+            href="https://github.com/GoblinDBRocks">GoblinDB</a>, <a href="https://github.com/rladies/news">Wikinews</a>,
+          <a href="https://github.com/Formula-UC3M">Telemetría Formula UC3M</a>, y <a
+            href="https://github.com/OSWeekends">muchos más</a>…</p>
+        <p>Además haremos varias Live Sessions de los Guilds para que podáis uniros al desarrollo de nuestros proyectos
+          Open Source durante el propio evento.</p>
+        <p>Por la tarde organizaremos charlas y talleres dentro de temáticas como Educación, programación FrontEnd,
+          programación funcional, Open Data… y mucho más, con spekaers tan potentes como <a
+            href="https://twitter.com/esStackOverflow">Juan Garza</a>, <a href="https://twitter.com/KoolTheba">Theba
+            Gomez</a>, <a href="https://twitter.com/IrnMM">Irene M. Morgado</a>, <a href="https://twitter.com/rubnvp">Rubén
+            Valseca</a>, <a href="https://twitter.com/kom_256">Ulises Gascón</a>, <a href="https://twitter.com/ancoar">Angel
+            Corral</a>…</p>
+        <p>Incluso tendremos un juego de hackers 👾(Capture the Flag/CTF) organizado por <a
+          href="https://twitter.com/hackmadrid">Hack Madrid %27</a>.</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'TextBlock',
+  };
+</script>
