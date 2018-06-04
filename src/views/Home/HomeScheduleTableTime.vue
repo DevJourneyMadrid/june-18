50e90b9701a33fcbd1519ce0a6da2f2b47a66d6f
trigoporres <santiagotrigoporres@gmail.com>
2018-06-04 15:55:41 +0200

    pequeñas modificaciones en tabla

--- src/views/Home/HomeScheduleTableTime.vue
+++ src/views/Home/HomeScheduleTableTime.vue
@@ -4,6 +4,7 @@
       <span>{{ el.time.start.hour }}</span>
       <small class="text-muted">:{{ el.time.start.min }}</small>
     </p>
+    <p class="lead hide"> - </p>
     <p class="m-0 lead">
       <span>{{ el.time.end.hour }}</span>
       <small class="text-muted">:{{ el.time.end.min }}</small>
@@ -32,5 +33,12 @@
     font-size 20px
     span
       font-weight 900
+  .hide
+    display none
+  @media (max-width: 500px)
+    p.lead
+      display inline
+    th
+      background-color #dfe2e7
 
 </style>
